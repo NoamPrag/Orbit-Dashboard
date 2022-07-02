15cd88a250abe3072a0421cb408f2199af791cc5
Noam Prag <noam.prag0@gmail.com>
2022-07-02 18:58:49 +0300

    mod for reading nt values

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,56 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+mod read;
 mod serializable_entry_value;
-use serializable_entry_value::SerializableEntryValue;
 
 mod connection;
 use connection::ConnectionState;
-
-use nt::{CallbackType, Client, EntryData, EntryValue, NetworkTables};
-use tauri::{window::Window, State};
-
-fn read_entry_value(entry_name: &str, client: &NetworkTables<Client>) -> Option<EntryValue> {
-    for (_, entry) in client.entries() {
-        if entry.name == entry_name {
-            return Some(entry.value);
-        }
-    }
-    None
-}
-
-#[tauri::command]
-async fn listen_to_entry<'a>(
-    entry_name: String,
-    window: Window,
-    conn_state: State<'_, ConnectionState>,
-    // TODO: create a struct to hold the Ok variant of the result
-) -> Result<(String, Option<SerializableEntryValue>), String> {
-    match conn_state.0.lock().unwrap().as_mut() {
-        Some(client) => {
-            let entry_name_clone: String = entry_name.clone(); // TODO: don't clone string
-
-            client.add_callback(CallbackType::Update, move |entry_data: &EntryData| {
-                if &entry_data.name != &entry_name_clone {
-                    return; // Don't send values of other entries
-                }
-
-                let serializable_value: SerializableEntryValue =
-                    SerializableEntryValue::wrap(entry_data.value.clone()); // TODO: figure out a way to not clone values
-                if let Err(err) = window.emit(&entry_name_clone, serializable_value) {
-                    println!("{}", err); // TODO: understand window emit error and do something appropriate
-                }
-            });
-
-            let entry_value: Option<SerializableEntryValue> =
-                read_entry_value(&entry_name, client).map(SerializableEntryValue::wrap);
-
-            Ok((entry_name, entry_value))
-        }
-
-        None => Err(String::from("Not connected to robot")),
-    }
-}
 
 fn main() {
     let context = tauri::generate_context!();
@@ -68,7 +23,7 @@ fn main() {
         .manage(ConnectionState::default())
         .invoke_handler(tauri::generate_handler![
             connection::connect,
-            listen_to_entry
+            read::listen_to_entry
         ])
         .run(context)
         .expect("error while running tauri application");
---
+++ src-tauri/src/read.rs
@@ -0,0 +1,48 @@
+use nt::{CallbackType, Client, EntryData, EntryValue, NetworkTables};
+use tauri::{State, Window};
+
+use crate::connection::ConnectionState;
+use crate::serializable_entry_value::SerializableEntryValue;
+
+fn read_entry_value(entry_name: &str, client: &NetworkTables<Client>) -> Option<EntryValue> {
+    for (_, entry) in client.entries() {
+        if entry.name == entry_name {
+            return Some(entry.value);
+        }
+    }
+    None
+}
+
+#[tauri::command]
+pub fn listen_to_entry<'a>(
+    entry_name: String,
+    window: Window,
+    conn_state: State<'_, ConnectionState>,
+    // TODO: create a struct to hold the Ok variant of the result
+) -> Result<(String, Option<SerializableEntryValue>), String> {
+    match conn_state.0.lock().unwrap().as_mut() {
+        Some(client) => {
+            let entry_name_clone: String = entry_name.clone(); // TODO: don't clone string
+
+            client.add_callback(CallbackType::Update, move |entry_data: &EntryData| {
+                if &entry_data.name != &entry_name_clone {
+                    return; // Don't send values of other entries
+                }
+
+                let serializable_value: SerializableEntryValue =
+                    SerializableEntryValue::wrap(entry_data.value.clone()); // TODO: figure out a way to not clone values
+
+                window
+                    .emit(&entry_name_clone, serializable_value)
+                    .unwrap_or_else(|err: tauri::Error| println!("{}", err));
+            });
+
+            let entry_value: Option<SerializableEntryValue> =
+                read_entry_value(&entry_name, client).map(SerializableEntryValue::wrap);
+
+            Ok((entry_name, entry_value))
+        }
+
+        None => Err(String::from("Not connected to robot")),
+    }
+}
