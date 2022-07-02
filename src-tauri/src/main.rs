a0e42defb449732c6e57fd4662f1881c2a91cd58
Noam Prag <noam.prag0@gmail.com>
2022-07-02 17:56:55 +0300

    Cleaned connection state code and entry value wrapper

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,27 +5,20 @@
 
 use std::sync::Mutex;
 
-use nt::{CallbackType, Client, ConnectionCallbackType, EntryData, EntryValue, NetworkTables};
+use nt::{CallbackType, Client, EntryData, EntryValue, NetworkTables};
 use tauri::{window::Window, State};
 
 #[derive(Default)]
 struct ConnState(Mutex<Option<NetworkTables<Client>>>);
 
-impl ConnState {
-    // TODO better name for function
-    async fn connect(ip: &str) -> nt::Result<NetworkTables<Client>> {
-        // ? client name
-        let client: NetworkTables<Client> = NetworkTables::connect(ip, "OrbitDashboard").await?;
-
-        client.add_connection_callback(ConnectionCallbackType::ClientDisconnected, |_| {
-            println!("Client connected!");
-        });
-        Ok(client)
-    }
-}
-
 #[derive(Clone)]
 struct EntryValueWrapper(EntryValue);
+
+impl EntryValueWrapper {
+    fn wrap(entry_value: EntryValue) -> Self {
+        EntryValueWrapper(entry_value)
+    }
+}
 
 impl serde::ser::Serialize for EntryValueWrapper {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -57,27 +50,27 @@ fn read_entry_value(entry_name: &str, client: &NetworkTables<Client>) -> Option<
 }
 
 #[tauri::command]
-fn listen_to_entry<'a>(
+async fn listen_to_entry<'a>(
     entry_name: String,
     window: Window,
     conn_state: State<'_, ConnState>,
-    // TODO create a struct to hold the Ok variant of the result
+    // TODO: create a struct to hold the Ok variant of the result
 ) -> Result<(String, Option<EntryValueWrapper>), String> {
     match conn_state.0.lock().unwrap().as_mut() {
         Some(client) => {
-            let entry_clone: String = entry_name.clone(); // TODO don't clone string
+            let entry_name_clone: String = entry_name.clone(); // TODO: don't clone string
 
             client.add_callback(CallbackType::Update, move |entry_data: &EntryData| {
-                let value: EntryValueWrapper = EntryValueWrapper(entry_data.value.clone()); // TODO figure out a way to not clone values
-                if let Err(err) = window.emit(&entry_name, value) {
-                    println!("{}", err); // TODO understand window emit error and do something appropriate
+                let value: EntryValueWrapper = EntryValueWrapper::wrap(entry_data.value.clone()); // TODO: figure out a way to not clone values
+                if let Err(err) = window.emit(&entry_name_clone, value) {
+                    println!("{}", err); // TODO: understand window emit error and do something appropriate
                 }
             });
 
-            let entry_value: Option<EntryValueWrapper> = read_entry_value(&entry_clone, client)
-                .map(|value: EntryValue| EntryValueWrapper(value));
+            let entry_value: Option<EntryValueWrapper> =
+                read_entry_value(&entry_name, client).map(EntryValueWrapper::wrap);
 
-            Ok((entry_clone, entry_value))
+            Ok((entry_name, entry_value))
         }
 
         None => Err(String::from("Not connected to robot")),
@@ -86,9 +79,9 @@ fn listen_to_entry<'a>(
 
 #[tauri::command]
 async fn connect(ip: String, conn_state: State<'_, ConnState>) -> Result<(), ()> {
-    // TODO convert nt::Error to string and return it in case of a failure
+    // TODO: convert nt::Error to string and return it in case of a failure
     let connected_state: Option<NetworkTables<Client>> =
-        Some(ConnState::connect(&ip).await.unwrap());
+        Some(NetworkTables::connect(&ip, "OrbitDashboard").await.unwrap());
     *conn_state.0.lock().unwrap() = connected_state;
     Ok(())
 }
