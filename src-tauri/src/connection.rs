f8ee8c6066684622d5c83e91fe71975bc6452f19
Noam Prag <noam.prag0@gmail.com>
2022-07-02 19:00:21 +0300

    Extracted constant for client name.

--- src-tauri/src/connection.rs
+++ src-tauri/src/connection.rs
@@ -6,11 +6,13 @@ use tauri::State;
 #[derive(Default)]
 pub struct ConnectionState(pub Mutex<Option<NetworkTables<Client>>>);
 
+const CLIENT_NAME: &str = "Orbit-Dashboard";
+
 #[tauri::command]
 pub async fn connect(ip: String, conn_state: State<'_, ConnectionState>) -> Result<(), ()> {
     // TODO: convert nt::Error to string and return it in case of a failure
     let connected_state: Option<NetworkTables<Client>> =
-        Some(NetworkTables::connect(&ip, "OrbitDashboard").await.unwrap());
+        Some(NetworkTables::connect(&ip, CLIENT_NAME).await.unwrap());
     *conn_state.0.lock().unwrap() = connected_state;
     Ok(())
 }
