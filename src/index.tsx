b41dac5284ba1bd9a01147bc589ae04e58c889d7
Noam Prag <noam.prag0@gmail.com>
2022-07-18 23:07:00 +0300

    Robot ip is constant

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 /* @refresh reload */
+import { invoke } from "@tauri-apps/api/tauri";
 import { render } from "solid-js/web";
 
 import App from "./App";
+
+// TODO ip should be picked by user
+const ROBOT_IP: string = "10.16.90.2:1735";
+invoke("connect", { ip: ROBOT_IP }).catch(console.error);
 
 const ROOT: HTMLElement = document.getElementById("root");
 
