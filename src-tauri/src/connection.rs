731a463f2a39042de1f747be2df6f79ea0c8a55e
Noam Prag <noam.prag0@gmail.com>
2022-07-19 13:50:19 +0300

    Returning a string in a case of a failure

--- src-tauri/src/connection.rs
+++ src-tauri/src/connection.rs
@@ -9,10 +9,13 @@ pub struct ConnectionState(pub Mutex<Option<NetworkTables<Client>>>);
 const CLIENT_NAME: &str = "Orbit-Dashboard";
 
 #[tauri::command]
-pub async fn connect(ip: String, conn_state: State<'_, ConnectionState>) -> Result<(), ()> {
-    // TODO: convert nt::Error to string and return it in case of a failure
-    let connected_state: Option<NetworkTables<Client>> =
-        Some(NetworkTables::connect(&ip, CLIENT_NAME).await.unwrap());
+pub async fn connect(ip: String, conn_state: State<'_, ConnectionState>) -> Result<(), String> {
+    let client = NetworkTables::connect(&ip, CLIENT_NAME).await;
+    if let Err(e) = client {
+        return Err(e.to_string());
+    }
+
+    let connected_state: Option<NetworkTables<Client>> = Some(client.unwrap());
     *conn_state.0.lock().unwrap() = connected_state;
     Ok(())
 }
