4b210791e963e8e232e539497845ccd4711e2395
Noam Prag <noam.prag0@gmail.com>
2022-07-25 08:58:57 +0300

    Cleaner match expression

--- src-tauri/src/write.rs
+++ src-tauri/src/write.rs
@@ -14,22 +14,21 @@ pub async fn set_entry_value(
     match conn_state.0.lock().await.as_mut() {
         Some(client) => {
             let value: EntryValue = value.into();
+
             match get_entry_id(&entry_name, client) {
                 // If an entry id exists, an entry with the given name already exists
-                Some(id) => client.update_entry(id, value),
-                None => {
-                    let entry_creation_result = client
-                        .create_entry(EntryData::new(entry_name, 0, value))
-                        .await;
-
-                    // TODO: handle error more properly
-                    if let Err(err) = entry_creation_result {
-                        return Err(err.to_string());
-                    }
+                Some(id) => {
+                    client.update_entry(id, value);
+                    Ok(())
                 }
-            };
-            Ok(())
+                None => client
+                    .create_entry(EntryData::new(entry_name, 0, value))
+                    .await
+                    .map(|_| {}) // Ignoring id
+                    .map_err(|e| e.to_string()), // TODO: handle error more properly
+            }
         }
+
         None => Err(String::from("Not connected to robot")),
     }
 }
